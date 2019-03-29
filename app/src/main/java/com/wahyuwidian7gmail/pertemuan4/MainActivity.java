2522a5f878d6bb2255a1e959f518617ba9c71f58
WidianWahyu <wahyuwidian7@gmail.com>
2019-03-29 11:09:28 +0700

    first commit

---
+++ app/src/main/java/com/wahyuwidian7gmail/pertemuan4/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wahyuwidian7gmail.pertemuan4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
